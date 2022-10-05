48a69d86fda3c5f8e91f6c7238370790210c4b98
AnantaSrikar <47946629+AnantaSrikar@users.noreply.github.com>
2022-10-05 18:29:05 +0530

    lab_1: Playfair cipher key matrix implemented

--- lab_1/encrypt_and_decrypt.cpp
+++ lab_1/encrypt_and_decrypt.cpp
@@ -1,3 +1,12 @@
+/*
+	Code to encrypt and decrypt text using the following ciphers
+		- Caesar's
+		- Vigenere
+		- Playfair
+
+	Author: Srikar
+*/
+
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -31,7 +40,7 @@ int main(int argc, char **argv)
 	int do_encrypt = atoi(argv[2]);
 	string KEY = argv[3];
 
-	if(!(alg_choice >= 0 && alg_choice <= 3))
+	if(!(alg_choice >= 0 && alg_choice <= 2))
 	{
 		cout<<"Invalid choice for encryption algorithm! Please go through README.md";
 		return -1;
@@ -66,22 +75,23 @@ int main(int argc, char **argv)
 	// Ze Function prototypes
 	void caesar(string, int, string);
 	void vigenere(string, int, string);
+	void playfair(string, int, string);
 	string strToUpper(string);
 
 	file_data =  strToUpper(file_data);
 
 	switch(alg_choice)
 	{
-		case 1:
+		case 0:
 				caesar(file_data, do_encrypt, KEY);
 				break;
 
-		case 2:
+		case 1:
 				vigenere(file_data, do_encrypt, KEY);
 				break;
 
-		case 3:
-				cout<<"Do da play fair Cipher!"<<endl;
+		case 2:
+				playfair(file_data, do_encrypt, KEY);
 				break;
 
 		default:
@@ -120,7 +130,7 @@ void caesar(string file_data, int do_encrypt, string KEY)
 	int KEY_VAL = stoi(KEY);
 
 	// Encryption
-	if(do_encrypt)
+	if(!do_encrypt)
 	{
 		cout<<"Encrypted text:"<<endl;
 
@@ -186,7 +196,7 @@ void vigenere(string file_data, int do_encrypt, string KEY)
 	int key_track = 0;
 
 	// Encryption
-	if(do_encrypt)
+	if(!do_encrypt)
 	{
 		cout<<"Encrypted text:"<<endl;
 
@@ -237,4 +247,136 @@ void vigenere(string file_data, int do_encrypt, string KEY)
 			key_track++;
 		}
 	}
+}
+
+// Function to check if a character is in the matrix
+int inMatrix(char matrix[5][5], char findChar)
+{
+	for(int i = 0; i < 5; i++)
+		for(int j = 0; j < 5; j++)
+			if(findChar == matrix[i][j] || (findChar == 'I' && matrix[i][j] == 'J') || (findChar == 'J' && matrix[i][j] == 'I'))	// Weird edge case for the cipher
+			{
+				// cout<<"Found "<<findChar<<endl;
+				return 1;
+			}
+
+	return 0;
+}
+
+void playfair(string file_data, int do_encrypt, string KEY)
+{
+	// Check for sus characters in KEY
+	for(int i = 0; i < KEY.length(); i++)
+	if(!isalpha(KEY[i]))
+		{
+			cout<<"Invalid key entered for Playfair Cipher! Please go through README.md";
+			exit(-1);
+		}
+
+	KEY = strToUpper(KEY);
+
+	cout<<KEY<<endl;
+	
+	char KEY_TABLE[5][5];
+
+	for(int i = 0; i < 5; i ++)
+		for(int j = 0; j < 5; j++)
+			KEY_TABLE[i][j] = '1';
+
+
+	int key_track = 0;
+
+	for(int i = 0; i < 5; i++)
+		for(int j = 0; j < 5; j++)
+		{
+			// Fill key_table with key characters
+			if(key_track < KEY.length())
+			{
+				// Skip character if already in matrix
+				while(inMatrix(KEY_TABLE, KEY[key_track]))
+					if(key_track < KEY.length())
+						key_track++;
+					else
+						break;
+
+				if(key_track < KEY.length())
+					KEY_TABLE[i][j] = KEY[key_track++];
+				else
+					goto REST_OF_THE_CHARS;
+			}
+
+			// Fill with rest of the alphabets
+			else
+			{
+				REST_OF_THE_CHARS:
+				for(char ch = 'A'; ch <= 'Z'; ch++)
+					if(inMatrix(KEY_TABLE, ch))
+						continue;
+					else
+					{
+						KEY_TABLE[i][j] = ch;
+						break;
+					}
+			}
+		}
+
+	for(int i = 0; i < 5; i++)
+	{
+		for(int j = 0; j < 5; j++)
+			cout<<KEY_TABLE[i][j]<<"\t";
+		cout<<endl;
+	}
+
+	// Encryption
+	// if(!do_encrypt)
+	// {
+	// 	cout<<"Encrypted text:"<<endl;
+
+	// 	// Iterating through all characters in the file.
+	// 	for(int i = 0; i < file_data.length(); i++)
+	// 	{
+	// 		// Check if character is an alphabet, print it directly if it isn't
+	// 		if(!isalpha(file_data[i]))
+	// 		{
+	// 			cout<<file_data[i];
+	// 			continue;
+	// 		}
+
+	// 		// Adding the key for the encryption
+	// 		char encpt_out = file_data[i] + (KEY[key_track % KEY.length()] % 26);
+
+	// 		// Rollover incase of value overflow
+	// 		while(encpt_out > 90)
+	// 			encpt_out -= 26;
+			
+	// 		cout<<encpt_out;
+	// 	}
+	// }
+
+	// // Decryption
+	// else
+	// {
+	// 	cout<<"Decrypted text:"<<endl;
+
+	// 	// Iterating through all characters in the file.
+	// 	for(int i = 0; i < file_data.length(); i++)
+	// 	{
+	// 		// Check if character is an alphabet, print it directly if it isn't
+	// 		if(!isalpha(file_data[i]))
+	// 		{
+	// 			cout<<file_data[i];
+	// 			continue;
+	// 		}
+
+	// 		// Adding the key for the encryption
+	// 		char encpt_out = file_data[i] - (KEY[key_track % KEY.length()] % 26);
+
+	// 		// Rollover incase of value overflow
+	// 		while(encpt_out < 65)
+	// 			encpt_out += 26;
+			
+	// 		cout<<encpt_out;
+	// 		key_track++;
+	// 	}
+	// }
 }
